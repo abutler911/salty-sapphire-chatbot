b10b7924f9961a6003ab942ea6b0566a0a4a665c
abutler911 <abutler911@gmail.com>
2023-07-23 21:15:49 -0600

    changed deploy config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
