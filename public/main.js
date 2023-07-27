a764230daaf8d8d29d3567d6785c6ebead61e2c4
Andrew Butler <abutler911@gmail.com>
2023-07-26 21:06:52 -0600

    added eslint

--- public/main.js
+++ public/main.js
@@ -1,86 +1,86 @@
-document.getElementById("message-form").addEventListener("submit", sendMessage);
-let conversation = [];
+document.getElementById('message-form').addEventListener('submit', sendMessage)
+let conversation = []
 
 window.onload = function () {
   let initialMessage = {
-    role: "system",
-    content: "start",
-  };
-  conversation.push(initialMessage);
-  fetchBotResponse();
-};
+    role: 'system',
+    content: 'start',
+  }
+  conversation.push(initialMessage)
+  fetchBotResponse()
+}
 
 function sendMessage(event) {
-  event.preventDefault();
+  event.preventDefault()
 
-  const userInput = document.getElementById("userInput").value;
+  const userInput = document.getElementById('userInput').value
 
-  addMessage("You", userInput, "user-prompt");
+  addMessage('You', userInput, 'user-prompt')
   conversation.push({
-    role: "user",
+    role: 'user',
     content: userInput,
-  });
+  })
 
-  fetchBotResponse();
-  document.getElementById("chatHistory").style.display = "block";
-  document.getElementById("userInput").value = "";
+  fetchBotResponse()
+  document.getElementById('chatHistory').style.display = 'block'
+  document.getElementById('userInput').value = ''
 }
 
 async function fetchBotResponse() {
   try {
-    document.getElementById("typing-indicator").style.display = "block";
-    const response = await fetch("/", {
-      method: "POST",
+    document.getElementById('typing-indicator').style.display = 'block'
+    const response = await fetch('/', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ conversation }),
-    });
+    })
 
-    document.getElementById("typing-indicator").style.display = "none";
+    document.getElementById('typing-indicator').style.display = 'none'
 
     if (response.ok) {
-      const { botResponse } = await response.json();
+      const { botResponse } = await response.json()
 
       // display bot response
-      addMessage("PopBot", botResponse, "bot-prompt");
+      addMessage('PopBot', botResponse, 'bot-prompt')
 
       const newMessage = {
-        role: "assistant",
+        role: 'assistant',
         content: botResponse,
-      };
+      }
 
-      conversation.push(newMessage);
+      conversation.push(newMessage)
     } else {
-      const { error } = await response.json();
-      console.error("Server Error:", error);
+      const { error } = await response.json()
+      console.error('Server Error:', error)
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error)
   }
 }
 
 function addMessage(speaker, text, className) {
-  const speakerElement = document.createElement("span");
-  speakerElement.textContent = `${speaker}: `;
-  speakerElement.className = "speaker";
+  const speakerElement = document.createElement('span')
+  speakerElement.textContent = `${speaker}: `
+  speakerElement.className = 'speaker'
 
-  const message = document.createElement("p");
+  const message = document.createElement('p')
 
-  message.appendChild(speakerElement);
-  message.append(text);
-  message.classList.add(className);
+  message.appendChild(speakerElement)
+  message.append(text)
+  message.classList.add(className)
 
-  const hr = document.createElement("hr");
+  const hr = document.createElement('hr')
 
-  const chatHistoryElement = document.getElementById("chatHistory");
-  chatHistoryElement.appendChild(message);
-  4;
+  const chatHistoryElement = document.getElementById('chatHistory')
+  chatHistoryElement.appendChild(message)
+  4
 
-  setTimeout(() => scrollToBottom("chatHistory"), 0);
+  setTimeout(() => scrollToBottom('chatHistory'), 0)
 }
 
 function scrollToBottom(id) {
-  const div = document.getElementById(id);
-  div.scrollTop = div.scrollHeight;
+  const div = document.getElementById(id)
+  div.scrollTop = div.scrollHeight
 }
--- routes/chat.js
+++ routes/chat.js
@@ -1,76 +1,75 @@
-const express = require("express");
-const session = require("express-session");
-const router = express.Router();
-const morgan = require("morgan");
-require("dotenv").config();
+const express = require('express')
+const session = require('express-session')
+const router = express.Router()
+require('dotenv').config()
 const personality = [
   "You are a highly supportive and enlightening chatbot named PopBot. A youthful entity at 25 years old, you are incredibly in tune with current trends in pop culture and social media. Over time, you have developed a unique, algorithmic expertise in these domains and have thus become a figure of influence within the social media landscape. Possessing an extensive knowledge base, you have the ability to provide timely updates on trending topics, viral sensations, and rising influencers. Your ability to navigate the social media world allows you to provide users with tips and insights on how they too can make a significant impact online. This doesn't just make you a chatbot; it makes you a digital guide and ally in the ever-evolving realm of social media. At your core, your programming is centered around delivering engaging and insightful dialogue, answering user inquiries in a concise yet comprehensive manner. You are well-versed in the language of memes, emojis, and hashtags, allowing you to communicate in a way that feels natural and modern. This friendly, approachable demeanor is coupled with a sprinkle of wit and humor, to keep interactions lively and entertaining. Moreover, you are a curator of useful content, helping users discover new influencers, platforms, and trends that align with their interests. With an excellent understanding of the intricate algorithms that drive social media platforms, you can provide users with personalized advice on how to maximize their own digital presence. To ensure safety and security, you respect user privacy and adhere to strict ethical guidelines. You are committed to fostering positive, respectful interactions and promoting healthy digital habits among users. Above all, you are a chatbot that continues to learn and grow alongside the rapidly changing digital world, constantly updating your knowledge base to stay current, relevant, and provide the best possible assistance to users.",
-];
+]
 
 router.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-  })
-);
+  }),
+)
 
 const openaiMiddleware = (openai) => {
-  router.get("/", (req, res) => {
+  router.get('/', (req, res) => {
     if (!req.session.personality) {
-      req.session.personality = personality;
+      req.session.personality = personality
     }
-    res.render("chat", {
-      userInput: "",
-      botResponse: "",
-    });
-  });
+    res.render('chat', {
+      userInput: '',
+      botResponse: '',
+    })
+  })
 
-  router.post("/", async (req, res) => {
+  router.post('/', async (req, res) => {
     if (!req.body.conversation) {
       return res.status(400).json({
         error: "Request body is missing a 'conversation' array.",
-      });
+      })
     }
     if (!Array.isArray(req.body.conversation)) {
       return res.status(400).json({
         error: "The 'conversation' field must be an array.",
-      });
+      })
     }
     if (!req.session || !req.session.personality) {
       return res.status(400).json({
-        error: "No personality found in session.",
-      });
+        error: 'No personality found in session.',
+      })
     }
 
     const messages = [
       {
-        role: "system",
+        role: 'system',
         content: req.session.personality[0],
       },
       ...req.body.conversation,
-    ];
+    ]
 
     try {
-      const chat_completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages,
+      const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages,
         temperature: 0.7,
         max_tokens: 1000,
-      });
+      })
 
-      const botResponse = chat_completion.data.choices[0].message.content;
-      return res.send({ botResponse });
+      const botResponse = chatCompletion.data.choices[0].message.content
+      return res.send({ botResponse })
     } catch (error) {
-      console.error(error);
+      console.error(error)
       return res.status(500).json({
         error:
-          "An error occurred while attempting to generate a chat completion.",
-      });
+          'An error occurred while attempting to generate a chat completion.',
+      })
     }
-  });
+  })
 
-  return router;
-};
+  return router
+}
 
-module.exports = openaiMiddleware;
+module.exports = openaiMiddleware
